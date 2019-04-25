c4e391c83c9addf9135d9217bda3d3afd4e27aa8
root <guilhermebromonschenkel@gmail.com>
2019-04-25 20:04:07 -0300

    app source code

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+//import { NavController } from 'ionic-angular';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +9,29 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  name: string;
 
+  constructor(private http: Http) {
+
+  }
+
+  checkName() {
+
+      let data = {
+          name: this.name
+      };
+
+      this.http.post('http://localhost:4000/checkname', data).pipe(
+          map(res => res.json())
+      ).subscribe(response => {
+          console.log('POST Response:', response);
+      });
+
+      this.http.get('http://localhost:4000/checkname/' + this.name).pipe(
+          map(res => res.json())
+      ).subscribe(response => {
+          console.log('GET Response:', response);
+      });
+
+  }
 }
