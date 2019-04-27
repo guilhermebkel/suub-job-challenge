c227bbdea69b631dd77a7477a8641e84a24f6c23
root <guilhermebromonschenkel@gmail.com>
2019-04-27 18:22:20 -0300

    Create, Read and Delete are working fine!

--- src/server/server.js
+++ src/server/server.js
@@ -14,11 +14,30 @@ const methodOverride = require('method-override')
 // Starts the restify server
 const server = restify.createServer();
 
+server.on('MethodNotAllowed', unknownMethodHandler);
+function unknownMethodHandler(req, res) {
+    if (req.method.toLowerCase() === 'options') {
+        console.log('received an options method request');
+      var allowHeaders = ['Accept', 'Accept-Version', 'Content-Type', 'Api-Version', 'Origin', 'X-Requested-With']; // added Origin & X-Requested-With
+  
+      if (res.methods.indexOf('OPTIONS') === -1) res.methods.push('OPTIONS');
+  
+      res.header('Access-Control-Allow-Credentials', true);
+      res.header('Access-Control-Allow-Headers', allowHeaders.join(', '));
+      res.header('Access-Control-Allow-Methods', res.methods.join(', '));
+      res.header('Access-Control-Allow-Origin', req.headers.origin);
+  
+      return res.send(204);
+    }
+    else
+      return res.send(new restify.MethodNotAllowedError());
+}
+
 // Connects to MongoDB Server
 mongoose.connect('mongodb+srv://guilherme:123@cluster0-zqwij.mongodb.net/test?retryWrites=true', { useNewUrlParser: true });
 
 // Hosts the server on a port
-server.listen(process.env.PORT || 4000, () => {
+server.listen(4000, () => {
     console.log('Server running at', server.name, server.url);
 })
 
